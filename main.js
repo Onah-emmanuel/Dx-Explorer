a408a1c21959f34b708f3d279d8b1b2c3dc8b468
Onah-emmanuel <onahemmanueluchenna@gmail.com>
2022-10-25 12:35:33 +0100

    phase complete

---
+++ main.js
@@ -0,0 +1,6 @@
+// console.log(document)
+var hamburgerdiv= document.querySelector(".hamburger")
+console.log(hamburgerdiv)
+function dosomething() {
+ hamburgerdiv.classList.toggle("showburger")
+}
