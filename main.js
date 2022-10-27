852564f7c19378db8fc7cc6fa073ad15e9d5dd9a
Onah-emmanuel <onahemmanueluchenna@gmail.com>
2022-10-27 16:45:43 +0100

    completed my project

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 
 
  var hamburger = document.querySelector(".hamburger-container")
-
+var mobile = document.querySelector(".mobile-links-holder")
+var mybackdrop = document.querySelector(".backdrop")
 console.log(hamburger)
 
 function addandremove() {
     hamburger.classList.toggle('showburger')
+    mobile.classList.toggle('show-mobile-links-holder')
+    mybackdrop.classList.toggle("show-backdrop")
 }
